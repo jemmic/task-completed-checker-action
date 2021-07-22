9451cde2e9bc26b9a3728dffb5421bccc02a2ac6
Christophe Kamphaus <christophe.kamphaus@jemmic.com>
2021-07-22 23:07:06 +0200

    Upgrade github actions to latest client version

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import {removeIgnoreTaskListText, createTaskListText} from './utils'
-import {Octokit} from '@octokit/rest'
+import {RestEndpointMethodTypes} from '@octokit/plugin-rest-endpoint-methods'
 
 async function run(): Promise<void> {
   try {
@@ -9,12 +9,12 @@ async function run(): Promise<void> {
 
     const token = core.getInput('repo-token', {required: true})
     const handleMissingTaskAsError = core.getBooleanInput('missing-as-error')
-    const githubApi = new github.GitHub(token)
+    const githubApi = github.getOctokit(token)
     const appName = 'Task Completed Checker'
 
     if (!body) {
       core.info('no task list and skip the process.')
-      await githubApi.checks.create({
+      await githubApi.rest.checks.create({
         name: appName,
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
         head_sha: github.context.payload.pull_request?.head.sha,
@@ -44,7 +44,7 @@ async function run(): Promise<void> {
     core.debug('creates a list of completed tasks and uncompleted tasks: ')
     core.debug(text)
 
-    const check: Octokit.RequestOptions & Octokit.ChecksCreateParams = {
+    const check: RestEndpointMethodTypes['checks']['create']['parameters'] = {
       name: appName,
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
       head_sha: github.context.payload.pull_request?.head.sha,
@@ -73,7 +73,7 @@ async function run(): Promise<void> {
       core.debug('Uncompleted tasks - mark as pending')
       check.status = 'in_progress'
     }
-    await githubApi.checks.create(check)
+    await githubApi.rest.checks.create(check)
   } catch (error) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     core.setFailed(error.message)
